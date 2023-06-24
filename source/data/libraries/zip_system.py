9b570ec8767a35cebc3b64833cbc7b85793ea7c7
qwertzuiii <78412852+qwertzuiii@users.noreply.github.com>
2023-06-24 13:16:19 +0200

    fixed that you could create a 6patch file that isn't a zip

--- source/data/libraries/zip_system.py
+++ source/data/libraries/zip_system.py
@@ -7,3 +7,10 @@ def extract_zip(zip_file, extract_folder):
 def make_extraction_folder(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+def is_zip_file(file_path):
+    try:
+        with zipfile.ZipFile(file_path) as zip_ref:
+            return True
+    except zipfile.BadZipFile:
+        return False
